304e0089240552b328ee2d07c7e6110e850db19c
Evren Kayali <evren.kayali@readify.net>
2019-01-06 23:04:46 +1100

    feat: add contacts to vendor form to save multiple contacts for a vendor

--- src/app/features/vendor/components/vendor-form/vendor-form.component.ts
+++ src/app/features/vendor/components/vendor-form/vendor-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Vendor } from '../../models/vendor.model';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,17 +16,40 @@ export class VendorFormComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
     private router: Router) {
-    this.vendorFormGroup = fb.group({
-      title: ['', Validators.required],
-      description: ['', Validators.compose([Validators.minLength(5), Validators.required])]
-    });
+
   }
 
   ngOnInit() {
+    this.vendorFormGroup = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.compose([Validators.minLength(5), Validators.required])],
+      contacts: this.fb.array([this.createContactItem()])
+    });
+  }
+
+  get Contacts() {
+    return this.vendorFormGroup.get('contacts') as FormArray;
+  }
+
+  addContact() {
+    this.Contacts.push(this.createContactItem());
+  }
+
+  createContactItem(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      email: '',
+      phone: '',
+    });
+  }
+
+  deleteContact(index: number) {
+    this.Contacts.removeAt(index);
   }
 
   submit() {
-    this.submitted.emit(Object.assign({}, this.vendorFormGroup.value))
+    this.vendor = Object.assign({}, this.vendorFormGroup.value)
+    this.submitted.emit(this.vendor);
   }
 
 }
--- src/app/features/vendor/vendor.module.ts
+++ src/app/features/vendor/vendor.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { VendorComponent } from './components/vendor/vendor.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { VendorFormComponent } from './components/vendor-form/vendor-form.component';
 import { VendorListComponent } from './components/vendor-list/vendor-list.component';
 import { VendorCardComponent } from './components/vendor-card/vendor-card.component';
@@ -10,11 +11,19 @@ import { VendorIndexComponent } from './components/vendor-index/vendor-index.com
 import { VendorSaveComponent } from './components/vendor-save/vendor-save.component';
 
 @NgModule({
-  declarations: [VendorComponent, VendorFormComponent, VendorListComponent, VendorCardComponent, VendorIndexComponent, VendorSaveComponent],
+  declarations: [
+    VendorComponent,
+    VendorFormComponent,
+    VendorListComponent,
+    VendorCardComponent,
+    VendorIndexComponent,
+    VendorSaveComponent
+  ],
   imports: [
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
+    NgbModule,
   ]
 })
 export class VendorModule { }
